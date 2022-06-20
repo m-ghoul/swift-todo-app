63cac3b96ae271e5dd741323ed8256dcdedcfbc3
Mohamed Ghoul <motheghoul@gmail.com>
2022-06-20 15:46:43 +0300

    Update

--- Tasks/Tasks/ViewController.swift
+++ Tasks/Tasks/ViewController.swift
@@ -18,7 +18,23 @@ class ViewController: UIViewController {
             UserDefaults().set(true, forKey: "setup")
             UserDefaults().set(0, forKey: "count")
         }
+        
+        addSiriButton(to: view)
     }
+    
+    func addSiriButton(to view: UIView) {
+            let button = INUIAddVoiceShortcutButton(style: .whiteOutline)
+            
+            button.shortcut = INShortcut(intent: intent )
+            button.delegate = self
+            
+            button.translatesAutoresizingMaskIntoConstraints = false
+            
+            view.addSubview(button)
+            view.centerXAnchor.constraint(equalTo: button.centerXAnchor).isActive = true
+            view.centerYAnchor.constraint(equalTo: button.bottomAnchor).isActive = true
+
+        }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewDidLoad()
@@ -30,6 +46,8 @@ class ViewController: UIViewController {
             self.tasks = taskList
             self.tableView.reloadData()
         }
+        
+        addSiriButton(to: view)
     }
     
     @IBAction func didTapAdd() {
@@ -57,5 +75,52 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         let task = tasks[indexPath.row]
         cell.textLabel?.text = task["title"] as? String
         return cell
+    }
+}
+
+extension ViewController: INUIAddVoiceShortcutButtonDelegate {
+    func present(_ addVoiceShortcutViewController: INUIAddVoiceShortcutViewController, for addVoiceShortcutButton: INUIAddVoiceShortcutButton) {
+        addVoiceShortcutViewController.delegate = self
+        addVoiceShortcutViewController.modalPresentationStyle = .formSheet
+        present(addVoiceShortcutViewController, animated: true, completion: nil)
+    }
+    
+    func present(_ editVoiceShortcutViewController: INUIEditVoiceShortcutViewController, for addVoiceShortcutButton: INUIAddVoiceShortcutButton) {
+        editVoiceShortcutViewController.delegate = self
+        editVoiceShortcutViewController.modalPresentationStyle = .formSheet
+        present(editVoiceShortcutViewController, animated: true, completion: nil)
+    }
+}
+
+extension ViewController: INUIAddVoiceShortcutViewControllerDelegate {
+    func addVoiceShortcutViewController(_ controller: INUIAddVoiceShortcutViewController, didFinishWith voiceShortcut: INVoiceShortcut?, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    func addVoiceShortcutViewControllerDidCancel(_ controller: INUIAddVoiceShortcutViewController) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension ViewController: INUIEditVoiceShortcutViewControllerDelegate {
+    func editVoiceShortcutViewController(_ controller: INUIEditVoiceShortcutViewController, didUpdate voiceShortcut: INVoiceShortcut?, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    func editVoiceShortcutViewController(_ controller: INUIEditVoiceShortcutViewController, didDeleteVoiceShortcutWithIdentifier deletedVoiceShortcutIdentifier: UUID) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    func editVoiceShortcutViewControllerDidCancel(_ controller: INUIEditVoiceShortcutViewController) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension ViewController {
+    public var intent: TaskTrackerIntent {
+        let intent = TaskTrackerIntent()
+        intent.title = "ADD TO SIRI TEST"
+        intent.suggestedInvocationPhrase = "I have something new to do"
+        return intent
     }
 }
