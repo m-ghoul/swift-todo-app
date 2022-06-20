3c09a056e8ce852ba93a240b6aa61eb0b64884ce
Mohamed Ghoul <motheghoul@gmail.com>
2022-06-20 14:52:11 +0300

    .

--- Tasks/Tasks/AppDelegate.swift
+++ Tasks/Tasks/AppDelegate.swift
@@ -28,4 +28,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
     }
 }
-
